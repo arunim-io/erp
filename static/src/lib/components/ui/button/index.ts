14b0fab8dadfc463d5d597d5323352e6f1e82770
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-01-31 19:02:33 +0600

    updates

--- static/src/lib/components/ui/button/index.ts
+++ static/src/lib/components/ui/button/index.ts
@@ -1,12 +1,7 @@
-import Root, {
-  type ButtonProps,
-  type ButtonSize,
-  type ButtonVariant,
-  buttonVariants,
-} from "./button.svelte";
+import type { ButtonProps, ButtonSize, ButtonVariant } from "./button.svelte";
+import Root, { buttonVariants } from "./button.svelte";
 
 export {
-  //
   Root as Button,
   type ButtonProps,
   type ButtonSize,
