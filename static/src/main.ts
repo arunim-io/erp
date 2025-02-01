f9c9ed983cf59028380209e462947db3b07953a7
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-02-01 13:59:40 +0600

    add tailwindcss & eslint

--- static/src/main.ts
+++ static/src/main.ts
@@ -1,14 +1,17 @@
 import { createInertiaApp } from "@inertiajs/vue3";
 import { createPinia } from "pinia";
 import { createApp, h } from "vue";
+import "./main.css";
 
 createInertiaApp({
-    async resolve(name) {
-        const pages = import.meta.glob("./pages/**/*.vue");
+  // eslint-disable-next-line ts/ban-ts-comment
+  // @ts-expect-error
+  async resolve(name) {
+    const pages = import.meta.glob("./pages/**/*.vue");
 
-        return await pages[`./pages/${name}.vue`]();
-    },
-    setup({ el, App, props }) {
-        createApp({ render: () => h(App, props) }).use(createPinia()).mount(el)
-    },
+    return await pages[`./pages/${name}.vue`]();
+  },
+  setup({ el, App, props }) {
+    createApp({ render: () => h(App, props) }).use(createPinia()).mount(el);
+  },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from "node:path";
 import { fileURLToPath, URL } from "node:url";
-
+import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
@@ -10,6 +10,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
