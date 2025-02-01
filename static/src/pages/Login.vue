199e2807b38a14799cb53b92318b8ae2a0e5df86
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-02-02 00:21:44 +0600

    add login form

--- static/src/pages/Login.vue
+++ static/src/pages/Login.vue
@@ -1,3 +1,50 @@
+<script setup lang="ts">
+import { Button } from "$src/components/ui/button";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "$src/components/ui/form";
+import { Input } from "$src/components/ui/input";
+import { toTypedSchema } from "@vee-validate/zod";
+import { useForm } from "vee-validate";
+import { z } from "zod";
+
+const formSchema = toTypedSchema(z.object({
+  username: z.string(),
+  password: z.string(),
+}));
+
+const form = useForm({ validationSchema: formSchema });
+
+const onSubmit = form.handleSubmit((values) => {
+  console.log("Form submitted!", values);
+});
+</script>
+
 <template>
-  LoginPage
+  <div class="py-10 space-y-5 container">
+    <h1 class="font-semibold text-3xl">
+      Login Page
+    </h1>
+    <form class="space-y-2.5" @submit="onSubmit">
+      <FormField v-slot="{ componentField }" name="username">
+        <FormItem>
+          <FormLabel>Username</FormLabel>
+          <FormControl>
+            <Input type="text" placeholder="johndoe@gmail.com" v-bind="componentField" />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <FormField v-slot="{ componentField }" name="password">
+        <FormItem>
+          <FormLabel>Password</FormLabel>
+          <FormControl>
+            <Input type="password" placeholder="**********" v-bind="componentField" />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <Button type="submit">
+        Login
+      </Button>
+    </form>
+  </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
 import { resolve } from "node:path";
 import { fileURLToPath, URL } from "node:url";
-import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       $src: fileURLToPath(new URL("./static/src", import.meta.url)),
