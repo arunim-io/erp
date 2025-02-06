a9838c36d4b7d12b8e1449bb40ec1822be51d554
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-02-06 14:45:14 +0600

    configure django-cotton

--- erp/settings.py
+++ erp/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_cotton",
 ]
 if DEBUG:
     INSTALLED_APPS += ["django_browser_reload"]
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [BASE_DIR / "static/"]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+COTTON_DIR = "components"
