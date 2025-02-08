9b4f29203e8f55c0d78169cc381bb8dbb98f3eb8
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-02-09 01:20:52 +0600

    add login page

--- erp/settings.py
+++ erp/settings.py
@@ -137,4 +137,6 @@ STATICFILES_DIRS = [BASE_DIR / "static/"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+LOGIN_REDIRECT_URL = "home"
+
 COTTON_DIR = "components"
--- erp/urls.py
+++ erp/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path("admin/docs/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-    path("", TemplateView.as_view(template_name="home.html")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
