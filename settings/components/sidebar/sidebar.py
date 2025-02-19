96526a92fee1e9b9a3fda24e58b63a3b5aa63d4c
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-02-19 22:12:50 +0600

    make settings sidebar collapsible

--- settings/components/sidebar/sidebar.py
+++ settings/components/sidebar/sidebar.py
@@ -1,6 +1,20 @@
+from django.http import HttpRequest
+from django.urls import reverse
 from django_components import Component, register
 
 
 @register("sidebar")
 class Sidebar(Component):
     template_file = "sidebar.html"
+    js_file = "sidebar.js"
+
+    def get_context_data(self, request: HttpRequest, *args, **kwargs):
+        return {
+            "links": [
+                {
+                    "label": "Personal Information",
+                    "icon_name": "mdi:user",
+                    "url": reverse("settings:user-info"),
+                },
+            ],
+        }
