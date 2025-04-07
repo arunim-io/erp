3ee1ce6cd19a09fc792b9d858e858ab28797b462
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-04-07 23:54:59 +0600

    configure django environs & mise secrets

--- backend/erp/settings.py
+++ backend/erp/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from environs import Env
+
+env = Env(prefix="DJANGO_")
+_ = env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ywd7^@*i%nyg735b7po=8g3@b6tjiubd0s_gr66o+k&67yl763"
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 
@@ -51,7 +56,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "erp.urls"
 
-TEMPLATES = [
+TEMPLATES = [  # pyright: ignore[reportUnknownVariableType]
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [],
