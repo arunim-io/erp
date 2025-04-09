92d53f95616200cd2e099ecb158454ab711f07f4
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-04-09 14:39:27 +0600

    organise config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import antfu from "@antfu/eslint-config";
 
 export default antfu({
+  ignores: ["./backend/", ".env.json"],
   stylistic: {
     quotes: "double",
     semi: true,
   },
   formatters: true,
+  toml: false,
   react: true,
 });
