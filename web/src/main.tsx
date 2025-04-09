9a65df9b8fdd8f6c5cb3c963829c96fd406a2af4
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-04-09 10:04:36 +0600

    web: cleanup

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root_element = document.getElementById("root");
+
+if (root_element != null) {
+  createRoot(root_element).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
